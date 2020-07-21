b991cf82b3b628667d758102efdd22957d02cd83
Mark Menshikh <menshikh.mg@esphere.ru>
2020-07-21 22:16:49 +0300

    Add methods for Delete and Edit users

--- src/WebAPI/Controllers/AdminController.cs
+++ src/WebAPI/Controllers/AdminController.cs
@@ -9,9 +9,9 @@ using WebAPI.Models;
 
 namespace WebAPI.Controllers
 {
+     /// <inheritdoc />
     [ApiController]
     [Route("[controller]")]
-    /// <inheritdoc />
     public class AdminController : ControllerBase
     {
         /// <summary>
@@ -25,21 +25,29 @@ namespace WebAPI.Controllers
         private readonly UserManager<AppUser> userManager;
 
         /// <summary>
+        /// Класс для хеширования паролей.
+        /// </summary>
+        private readonly IPasswordHasher<AppUser> passwordНasher;
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="AdminController"/> class.
         /// </summary>
         /// <param name="mapper">Класс для маппинга.</param>
         /// <param name="usrMgr">API для управления пользователями.</param>
-        public AdminController(IMapper mapper, UserManager<AppUser> usrMgr)
+        public AdminController(IPasswordHasher<AppUser> passwordHash, IMapper mapper, UserManager<AppUser> usrMgr)
         {
             userManager = usrMgr;
             _mapper = mapper;
+            passwordНasher = passwordHash;
         }
 
         /// <summary>
         /// Создание пользователя.
         /// </summary>
         /// <param name="model"></param>
-        /// <returns><placeholder>A <see cref="Task"/> representing the asynchronous operation.</placeholder></returns>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        [HttpPost]
+        [Route("create")]
         public async Task<CreateModel> Create(CreateModel model)
         {
             if (ModelState.IsValid)
@@ -63,9 +71,62 @@ namespace WebAPI.Controllers
         }
 
         /// <summary>
+        /// Удаление пользователя с данным id.
+        /// </summary>
+        /// <param name="id">Id удаляемого пользователя</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        [HttpPost]
+        [Route("{id}")]
+        public async Task<AppUser> Delete(string id)
+        {
+            var user = await userManager.FindByIdAsync(id).ConfigureAwait(true);
+            if (user != null)
+            {
+                var result = await userManager.DeleteAsync(user);
+                if (result.Succeeded)
+                {
+                    return user;
+                } else
+                {
+                    return null;
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Редактирование пользователя.
+        /// </summary>
+        /// <param name="model">Модель на основе которой происходят изменения</param>
+        /// <returns>Измененный пользователь.</returns>
+        [HttpPost]
+        [Route("edit")]
+        public async Task<AppUser> Edit(CreateModel model)
+        {
+            var user = await userManager.FindByNameAsync(model.Name).ConfigureAwait(true);
+            if (user != null)
+            {
+                user.Email = model.Email;
+                user.PasswordHash = passwordНasher.HashPassword(user, model.Password);
+                var result = await userManager.UpdateAsync(user).ConfigureAwait(true);
+                if (result.Succeeded)
+                {
+                    return user;
+                } else
+                {
+                    return null;
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
         /// Возвращает список пользователей.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>Список пользователей</returns>
+        [Route("index")]
         public List<AppUser> Index()
         {
             return userManager.Users.ToList();
